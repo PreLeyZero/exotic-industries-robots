11e10dd69277d1f41a1183f083731b733963c6ef
PreLeyZero <Paul.Zschoppe@gmx.de>
2023-01-25 01:49:50 +0100

    init

---
+++ prototypes/exotic-robots.lua
@@ -0,0 +1,8 @@
+-- retrun if exotic industries is not present
+if not mods["exotic-industries"] then return end
+
+ei_robots_lib = require("lib/lib-data")
+
+--====================================================================================================
+--ROBOT CHANGES
+--====================================================================================================
